2b33082d845fd56f001ff480354ef3f8833a7791
Vidushi2797 <33202448+Vidushi2797@users.noreply.github.com>
2022-09-18 15:06:40 -0700

    Update user-data-script.sh

--- web-tier-server/user-data-script.sh
+++ web-tier-server/user-data-script.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install nodejs -y
 sudo apt install npm -y
-git clone https://github.com/mahathiamencherla/CSE546-IaaS-Proj1.git
+git clone https://github.com/mahathiamencherla/CSE546-IaaS-Proj1.git /home/ubuntu/CSE546-IaaS-Proj1
 cd CSE546-IaaS-Proj1/web-tier-server
 node web-tier-server.js 
