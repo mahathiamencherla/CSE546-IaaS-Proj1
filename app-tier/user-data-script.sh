d81632ec2871ac3c0edb64be544cf537978ae998
Vidushi2797 <33202448+Vidushi2797@users.noreply.github.com>
2022-09-18 14:19:53 -0700

    Create user-data-script.sh

---
+++ app-tier/user-data-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/mahathiamencherla/CSE546-IaaS-Proj1.git
+pip3 install torch==1.5.1+cpu torchvision==0.6.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install boto3
