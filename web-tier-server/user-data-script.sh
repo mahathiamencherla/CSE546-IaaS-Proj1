0ab61fa9c368b8307ea4628fff8b45b1e1236fc6
Siddhant Sorann <siddhantsorann@gmail.com>
2022-09-19 16:10:02 -0700

    updated web tier user data script for cloudwatch metrics

--- web-tier-server/user-data-script.sh
+++ web-tier-server/user-data-script.sh
@@ -2,16 +2,19 @@
 sudo apt update
 sudo apt install nodejs -y
 sudo apt install npm -y
+sudo apt install python3-pip -y
 git clone https://github.com/mahathiamencherla/CSE546-IaaS-Proj1.git /home/ubuntu/CSE546-IaaS-Proj1
 cd /home/ubuntu
 sudo chown -R ubuntu CSE546-IaaS-Proj1
 cd CSE546-IaaS-Proj1
-echo $'AWS_KEY=<KEY>\nAWS_SECRET=<SECRET>' > key.env
+echo $'AWS_KEY=AKIAZ23MWYXILHDPNYHB\nAWS_SECRET=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' > key.env
 sudo npm install pm2@latest -g
 cd web-tier-server
 sudo npm install
+pip3 install boto3 --no-cache-dir
 sudo -u ubuntu -i <<'EOF'
 pm2 startup
 pm2 start node -- CSE546-IaaS-Proj1/web-tier-server/web-tier-server.js
+pm2 start CSE546-IaaS-Proj1/web-tier-server/cloudwatch.py --interpreter python3 --restart-delay 10000
 pm2 save
 EOF
