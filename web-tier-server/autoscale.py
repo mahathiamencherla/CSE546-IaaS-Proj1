88292d91bbf136a352d4ca043ad987abe9fc0cf7
Siddhant Sorann <siddhantsorann@gmail.com>
2022-09-23 18:04:31 -0700

    updated autoscale.py

--- web-tier-server/autoscale.py
+++ web-tier-server/autoscale.py
@@ -2,6 +2,7 @@ import boto3
 import os
 from dotenv import load_dotenv
 import math
+import time
 
 load_dotenv('../key.env')
 
@@ -11,54 +12,74 @@ responseQueue = boto3.client('sqs', region_name="us-east-1",
 ec2 = boto3.client('ec2', region_name="us-east-1",
         aws_access_key_id=os.environ.get('AWS_KEY'),
                        aws_secret_access_key=os.environ.get('AWS_SECRET'))
-# Get the queue URL
-requestQueue_url = 'https://sqs.us-east-1.amazonaws.com/246156685396/RequestQueue'
-# Fetch the ApproximateNumberOfMessages from the queue
-requestQueue_attributes = responseQueue.get_queue_attributes(QueueUrl=requestQueue_url, AttributeNames=['ApproximateNumberOfMessages'])
-noOfMessages = int(requestQueue_attributes['Attributes']['ApproximateNumberOfMessages'])
-# Fetch ids of the running instances
-runningEc2 = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running','pending']},{'Name': 'tag:tier', 'Values': ['app']}])
-runningEc2Ids = []
-# List of IDs of running instances
-if runningEc2['Reservations']:
-        for instance in runningEc2['Reservations'][0]['Instances']:
-                runningEc2Ids.append(instance['InstanceId'])
-print(noOfMessages,runningEc2Ids)
-# Total number of instance required for the messages in the queue
-target = 50
-requiredEc2 = min(math.ceil(noOfMessages/target), 20)
-if requiredEc2 == 0:
-        requiredEc2 = 1
-print("required = " + str(requiredEc2))
-noOfRunningEc2 = len(runningEc2Ids)
-print("running = " + str(noOfRunningEc2))
+zeroInstances = False
+zero_time = time.time()
+def run():
+        # Get the queue URL
+        requestQueue_url = 'https://sqs.us-east-1.amazonaws.com/246156685396/RequestQueue'
+        # Fetch the ApproximateNumberOfMessages from the queue
+        requestQueue_attributes = responseQueue.get_queue_attributes(QueueUrl=requestQueue_url, AttributeNames=['ApproximateNumberOfMessages'])
+        noOfMessages = int(requestQueue_attributes['Attributes']['ApproximateNumberOfMessages'])
+        # Fetch ids of the running instances
+        runningEc2 = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running','pending']},{'Name': 'tag:tier', 'Values': ['app']}])
+        runningEc2Ids = []
+        # List of IDs of running instances
+        if runningEc2['Reservations']:
+                for instance in runningEc2['Reservations'][0]['Instances']:
+                        runningEc2Ids.append(instance['InstanceId'])
+        print(noOfMessages,runningEc2Ids)
+        # Total number of instance required for the messages in the queue
+        target = 50
+        requiredEc2 = min(math.ceil(noOfMessages/target), 20)
+        print("required = " + str(requiredEc2))
+        noOfRunningEc2 = len(runningEc2Ids)
+        print("running = " + str(noOfRunningEc2))
+        if requiredEc2 == 0 and noOfRunningEc2 > 0:
+                if zeroInstances:
+                        mins = (time.time() - zero_time) // 60
+                        if mins < 2:
+                              requiredEc2 = 1
+                        print("Minutes elapsed since zero instances required: " + str(mins))
+                else:
+                        zero_time = time.time()
+                        zeroInstances = True
+                        requiredEc2 = 1
+                        print("First time zero instances required")
+                print("New required count: " + requiredEc2)
+        if zeroInstances and requiredEc2 > 0:
+                zeroInstances = False
+        user_data = '''#!/bin/bash
+        sudo -u ubuntu -i <<'EOF'
+        pm2 resurrect
+        EOF'''
 
-user_data = '''#!/bin/bash
-sudo -u ubuntu -i <<'EOF'
-pm2 resurrect
-EOF'''
-
-# Autoscaling
-if noOfRunningEc2 == requiredEc2:
-        print("Autoscaling not required!")
-# Scaling-in
-elif noOfRunningEc2>requiredEc2:
-        remove = noOfRunningEc2 - requiredEc2
-        print(ec2.terminate_instances(InstanceIds=runningEc2Ids[:remove]))
-# Scaling-out
-else:
-        add = requiredEc2 - noOfRunningEc2
-        response=ec2.run_instances(ImageId='ami-02a102f25a59e9a46',InstanceType='t2.micro',KeyName='cloud-proj',MinCount=add,MaxCount=add,UserData=user_data,TagSpecifications=[{
-                                'ResourceType': 'instance','Tags': [
-                                {
-                                        'Key': 'tier',
-                                        'Value': 'app'
+        # Autoscaling
+        if noOfRunningEc2 == requiredEc2:
+                print("Autoscaling not required!")
+        # Scaling-in
+        elif noOfRunningEc2>requiredEc2:
+                remove = noOfRunningEc2 - requiredEc2
+                print(ec2.terminate_instances(InstanceIds=runningEc2Ids[:remove]))
+        # Scaling-out
+        else:
+                add = requiredEc2 - noOfRunningEc2
+                response=ec2.run_instances(ImageId='ami-02a102f25a59e9a46',InstanceType='t2.micro',KeyName='cloud-proj',MinCount=add,MaxCount=add,UserData=user_data,TagSpecifications=[{
+                                        'ResourceType': 'instance','Tags': [
+                                        {
+                                                'Key': 'tier',
+                                                'Value': 'app'
+                                        },
+                                        {
+                                                'Key': 'Name',
+                                                'Value': 'app-tier'
+                                        },
+                                        ]
                                 },
-                                {
-                                        'Key': 'Name',
-                                        'Value': 'app-tier'
-                                },
-                                ]
-                        },
-                        ])
-        print(response)
+                                ])
+                print(response)
+                
+if __name__ == "__main__":
+        while True:
+                run()
+                time.sleep(10)
+                
